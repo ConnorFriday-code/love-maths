742ddaff4e5d42e3d23731889edb37633a22a1fa
Connor Friday <connor.friday.cf@gmail.com>
2024-03-23 03:51:59 +0000

    JavaScript: Creating the score increase system for both correct and incorrect answers.

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,8 +42,10 @@ function checkAnswer (){
 
     if(isCorrect){
         alert("Correct!")
+        incrementScore();
     } else{
         alert(`Sadly, you are incorrect. You answered ${userAnswer}, the correct answer is ${calculatedAnswer[0]}.`)
+        incrementWrongAnswer();
     }
 }
 
@@ -64,12 +66,20 @@ function calculateCorrectAnswer (){
     }
 }
 
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
 function incrementScore (){
-
+    let oldScore=parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText=++oldScore;
 }
 
+/**
+ * Gets the current incorrect score from the DOM and increments it by 1
+ */
 function incrementWrongAnswer (){
-
+    let oldScore=parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText=++oldScore;
 }
 
 function displayAdditionQuestion (operand1, operand2){
